945444f85d14333293958e684d623ee476c3ff8a
Wendy <wendyowino18@gmail.com>
2024-11-10 00:19:10 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueProgressBar from "@aacassandra/vue3-progressbar";
 
 createApp(App).mount('#app')
+
+const options = {
+    color: "#bffaf3",
+    failedColor: "#874b4b",
+    thickness: "5px",
+    transition: {
+      speed: "0.2s",
+      opacity: "0.6s",
+      termination: 300,
+    },
+    autoRevert: true,
+    location: "left",
+    inverse: false,
+  };
+  
+  createApp(App)
+    .use(VueProgressBar, options)
+    .use(router)
+    .mount("#app");
